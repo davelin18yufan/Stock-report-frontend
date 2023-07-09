f8db35e1408bc260f979b85900febb98d320c071
davelin18yufan <davelin30630@gmail.com>
2023-07-09 15:05:29 +0800

    refactor: refactor router

--- src/pages/stock/index.jsx
+++ src/pages/stock/index.jsx
@@ -1,19 +1,23 @@
 import { MainContainer, Footer, Header, Navbar, ScrollToTopBtn } from "../../components"
 import { MainSector } from "./element"
+import { Outlet } from "react-router-dom"
 
 
 const StockPage = () => {
   return(
-    <MainContainer>
-      <Header />
-      <div className="h-full flex flex-col sm:flex-row  dark:bg-slate-800 ">
-        <Navbar />
-        <MainSector />
-      </div>
-      <Footer />
-      <ScrollToTopBtn />
-    </MainContainer>
-)
+    <>
+      <Outlet />
+      <MainContainer>
+        <Header />
+        <div className="h-full flex flex-col sm:flex-row  dark:bg-slate-800 ">
+          <Navbar />
+          <MainSector />
+        </div>
+        <Footer />
+        <ScrollToTopBtn />
+      </MainContainer>
+    </>
+  )
 }
 
 export default StockPage
--- src/routes.jsx
+++ src/routes.jsx
@@ -11,61 +11,61 @@ import AdminList from "./pages/admin/list"
 import UserPage from "./pages/user"
 
 const routes = [
-    {
-        path: "*",
-        element: <HomePage />,
-        children: []
-    },
-    {
-        path: "/login",
-        element: <Login />,
-        children: []
-    },
-    {
-        path: "/register",
-        element: <Register />,
-        children: []
-    },
-    {
-        path: "/setting",
-        element: <Setting />,
-        children: []
-    },
-    {
-        path: "/main",
-        element: <MainPage />,
-        children: []
-    },
-    {
-        path: "/stock/:symbol",
-        element: <SingleStockPage />,
-        children: []
-    },
-    {
-        path: "/stock",
-        element: <StockPage />,
-        children: []
-    },
-    {
-        path: "/user/:userId",
-        element: <UserPage />,
-        children: []
-    },
-    {
-        path: "/admin/list",
-        element: <AdminList />,
-        children: []
-    },
-    {
-        path: "/admin/login",
-        element: <AdminLogin />,
-        children: []
-    },
-    {
-        path: "/admin/users",
-        element: <AdminUser />,
-        children: []
-    }
-]
+  {
+    path: "/login",
+    element: <Login />,
+    children: [],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    children: [],
+  },
+  {
+    path: "/setting",
+    element: <Setting />,
+    children: [],
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+    children: [],
+  },
+  {
+    path: "/stock",
+    element: <StockPage />,
+    children: [
+        {
+          path: "/stock/:symbol",
+          element: <SingleStockPage />,
+        },
+    ],
+  },
+  {
+    path: "/user/:userId",
+    element: <UserPage />,
+    children: [],
+  },
+  {
+    path: "/admin/list",
+    element: <AdminList />,
+    children: [],
+  },
+  {
+    path: "/admin/login",
+    element: <AdminLogin />,
+    children: [],
+  },
+  {
+    path: "/admin/users",
+    element: <AdminUser />,
+    children: [],
+  },
+  {
+    path: "*",
+    element: <HomePage />,
+    children: [],
+  }
+];
 
 export default routes
